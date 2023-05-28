132a089814ac5288600968cafdae8ccba71c1aa4
Nosalis1 <aleksa9192@gmail.com>
2023-05-28 14:32:04 +0200

    Quick Fix

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import game.users.User;
+import util.events.ArgEvent;
+import util.events.Event;
+
 public class Main {
     public static void main(String[] args) {
+
         util.Console.message("Hello World!");
 
         RoomManager.initialize();
